fbc0547a2e02f39985d16432ed7e4e49d0a3aebf
Rishabh <rishabhm.10.03@gmail.com>
2024-09-11 10:29:20 +0530

    created design for login and admin panel

--- village-management/village-management/UserPanel.Designer.cs
+++ village-management/village-management/UserPanel.Designer.cs
@@ -28,12 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "UserPanel";
+            components = new System.ComponentModel.Container();
+            panel1 = new Panel();
+            bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(components);
+            bunifuElipse2 = new Bunifu.Framework.UI.BunifuElipse(components);
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            linkLabel3 = new LinkLabel();
+            linkLabel4 = new LinkLabel();
+            linkLabel5 = new LinkLabel();
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Snow;
+            panel1.Location = new Point(204, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(793, 574);
+            panel1.TabIndex = 0;
+            // 
+            // bunifuElipse1
+            // 
+            bunifuElipse1.ElipseRadius = 35;
+            bunifuElipse1.TargetControl = this;
+            // 
+            // bunifuElipse2
+            // 
+            bunifuElipse2.ElipseRadius = 35;
+            bunifuElipse2.TargetControl = panel1;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Century Gothic", 15F, FontStyle.Bold);
+            linkLabel1.LinkColor = Color.Black;
+            linkLabel1.Location = new Point(43, 162);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(69, 23);
+            linkLabel1.TabIndex = 1;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Home";
+            linkLabel1.VisitedLinkColor = Color.FromArgb(192, 255, 255);
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Font = new Font("Century Gothic", 15F, FontStyle.Bold);
+            linkLabel2.LinkColor = Color.Black;
+            linkLabel2.Location = new Point(43, 220);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(69, 23);
+            linkLabel2.TabIndex = 2;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Profile";
+            linkLabel2.VisitedLinkColor = Color.FromArgb(192, 255, 255);
+            // 
+            // linkLabel3
+            // 
+            linkLabel3.AutoSize = true;
+            linkLabel3.Font = new Font("Century Gothic", 15F, FontStyle.Bold);
+            linkLabel3.LinkColor = Color.Black;
+            linkLabel3.Location = new Point(43, 278);
+            linkLabel3.Name = "linkLabel3";
+            linkLabel3.Size = new Size(121, 23);
+            linkLabel3.TabIndex = 3;
+            linkLabel3.TabStop = true;
+            linkLabel3.Text = "Complaints";
+            linkLabel3.VisitedLinkColor = Color.FromArgb(192, 255, 255);
+            // 
+            // linkLabel4
+            // 
+            linkLabel4.AutoSize = true;
+            linkLabel4.Font = new Font("Century Gothic", 15F, FontStyle.Bold);
+            linkLabel4.LinkColor = Color.Black;
+            linkLabel4.Location = new Point(43, 336);
+            linkLabel4.Name = "linkLabel4";
+            linkLabel4.Size = new Size(77, 23);
+            linkLabel4.TabIndex = 4;
+            linkLabel4.TabStop = true;
+            linkLabel4.Text = "Search";
+            linkLabel4.VisitedLinkColor = Color.FromArgb(192, 255, 255);
+            // 
+            // linkLabel5
+            // 
+            linkLabel5.AutoSize = true;
+            linkLabel5.Font = new Font("Century Gothic", 15F, FontStyle.Bold);
+            linkLabel5.LinkColor = Color.Black;
+            linkLabel5.Location = new Point(43, 394);
+            linkLabel5.Name = "linkLabel5";
+            linkLabel5.Size = new Size(75, 23);
+            linkLabel5.TabIndex = 5;
+            linkLabel5.TabStop = true;
+            linkLabel5.Text = "Notice";
+            linkLabel5.VisitedLinkColor = Color.FromArgb(192, 255, 255);
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 15F, FontStyle.Bold);
+            label1.Location = new Point(43, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 23);
+            label1.TabIndex = 6;
+            label1.Text = "Username";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(43, 486);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 31);
+            button1.TabIndex = 0;
+            button1.Text = "Logout";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // UserPanel
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSteelBlue;
+            ClientSize = new Size(1009, 598);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(linkLabel5);
+            Controls.Add(linkLabel4);
+            Controls.Add(linkLabel3);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "UserPanel";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "UserPanel";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse2;
+        private Label label1;
+        private LinkLabel linkLabel5;
+        private LinkLabel linkLabel4;
+        private LinkLabel linkLabel3;
+        private LinkLabel linkLabel2;
+        private LinkLabel linkLabel1;
+        private Button button1;
     }
 }
