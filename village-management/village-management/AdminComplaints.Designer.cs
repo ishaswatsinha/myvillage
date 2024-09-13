bd54514f924bbfac47dc419daff587f221cbf057
Rishabh <rishabhm.10.03@gmail.com>
2024-09-14 00:07:16 +0530

    added all navbar pages for admin

--- village-management/village-management/AdminComplaints.Designer.cs
+++ village-management/village-management/AdminComplaints.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace village_management
 {
-    partial class AdminPanel
+    partial class AdminComplaints
     {
         /// <summary>
         /// Required designer variable.
@@ -31,14 +31,13 @@
             components = new System.ComponentModel.Container();
             bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(components);
             panel1 = new Panel();
-            button1 = new Button();
+            bunifuElipse2 = new Bunifu.Framework.UI.BunifuElipse(components);
             label1 = new Label();
+            dashboard = new LinkLabel();
+            backmenu = new Panel();
             linkLabel4 = new LinkLabel();
             linkLabel3 = new LinkLabel();
             linkLabel2 = new LinkLabel();
-            dashboard = new LinkLabel();
-            bunifuElipse2 = new Bunifu.Framework.UI.BunifuElipse(components);
-            backmenu = new Panel();
             bunifuElipse3 = new Bunifu.Framework.UI.BunifuElipse(components);
             SuspendLayout();
             // 
@@ -50,20 +49,15 @@
             // panel1
             // 
             panel1.BackColor = Color.Snow;
-            panel1.Location = new Point(225, 12);
+            panel1.Location = new Point(224, 12);
             panel1.Name = "panel1";
-            panel1.Size = new Size(733, 449);
+            panel1.Size = new Size(734, 449);
             panel1.TabIndex = 0;
             // 
-            // button1
+            // bunifuElipse2
             // 
-            button1.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(35, 484);
-            button1.Name = "button1";
-            button1.Size = new Size(107, 31);
-            button1.TabIndex = 7;
-            button1.Text = "Logout";
-            button1.UseVisualStyleBackColor = true;
+            bunifuElipse2.ElipseRadius = 35;
+            bunifuElipse2.TargetControl = panel1;
             // 
             // label1
             // 
@@ -72,8 +66,31 @@
             label1.Location = new Point(35, 69);
             label1.Name = "label1";
             label1.Size = new Size(140, 23);
-            label1.TabIndex = 13;
+            label1.TabIndex = 18;
             label1.Text = "Admin Name";
+            // 
+            // dashboard
+            // 
+            dashboard.AutoSize = true;
+            dashboard.BackColor = Color.Transparent;
+            dashboard.Font = new Font("Century Gothic", 15F, FontStyle.Bold);
+            dashboard.LinkColor = Color.Black;
+            dashboard.Location = new Point(22, 167);
+            dashboard.Name = "dashboard";
+            dashboard.Size = new Size(116, 23);
+            dashboard.TabIndex = 19;
+            dashboard.TabStop = true;
+            dashboard.Text = "Dashboard";
+            dashboard.VisitedLinkColor = Color.FromArgb(192, 255, 255);
+            // 
+            // backmenu
+            // 
+            backmenu.BackColor = Color.CadetBlue;
+            backmenu.Location = new Point(11, 228);
+            backmenu.Margin = new Padding(2);
+            backmenu.Name = "backmenu";
+            backmenu.Size = new Size(168, 37);
+            backmenu.TabIndex = 23;
             // 
             // linkLabel4
             // 
@@ -83,7 +100,7 @@
             linkLabel4.Location = new Point(22, 349);
             linkLabel4.Name = "linkLabel4";
             linkLabel4.Size = new Size(163, 23);
-            linkLabel4.TabIndex = 11;
+            linkLabel4.TabIndex = 22;
             linkLabel4.TabStop = true;
             linkLabel4.Text = "See Complaints";
             linkLabel4.VisitedLinkColor = Color.FromArgb(192, 255, 255);
@@ -96,7 +113,7 @@
             linkLabel3.Location = new Point(22, 291);
             linkLabel3.Name = "linkLabel3";
             linkLabel3.Size = new Size(110, 23);
-            linkLabel3.TabIndex = 10;
+            linkLabel3.TabIndex = 21;
             linkLabel3.TabStop = true;
             linkLabel3.Text = "Put Notice";
             linkLabel3.VisitedLinkColor = Color.FromArgb(192, 255, 255);
@@ -104,68 +121,38 @@
             // linkLabel2
             // 
             linkLabel2.AutoSize = true;
+            linkLabel2.BackColor = Color.CadetBlue;
             linkLabel2.Font = new Font("Century Gothic", 15F, FontStyle.Bold);
             linkLabel2.LinkColor = Color.Black;
             linkLabel2.Location = new Point(22, 233);
             linkLabel2.Name = "linkLabel2";
             linkLabel2.Size = new Size(148, 23);
-            linkLabel2.TabIndex = 9;
+            linkLabel2.TabIndex = 20;
             linkLabel2.TabStop = true;
             linkLabel2.Text = "Manage Users";
             linkLabel2.VisitedLinkColor = Color.FromArgb(192, 255, 255);
-            // 
-            // dashboard
-            // 
-            dashboard.AutoSize = true;
-            dashboard.BackColor = Color.CadetBlue;
-            dashboard.Font = new Font("Century Gothic", 15F, FontStyle.Bold);
-            dashboard.LinkColor = Color.Black;
-            dashboard.Location = new Point(22, 167);
-            dashboard.Name = "dashboard";
-            dashboard.Size = new Size(116, 23);
-            dashboard.TabIndex = 8;
-            dashboard.TabStop = true;
-            dashboard.Text = "Dashboard";
-            dashboard.VisitedLinkColor = Color.FromArgb(192, 255, 255);
-            dashboard.LinkClicked += dashboard_LinkClicked;
-            // 
-            // bunifuElipse2
-            // 
-            bunifuElipse2.ElipseRadius = 35;
-            bunifuElipse2.TargetControl = panel1;
-            // 
-            // backmenu
-            // 
-            backmenu.BackColor = Color.CadetBlue;
-            backmenu.Location = new Point(12, 162);
-            backmenu.Margin = new Padding(2, 2, 2, 2);
-            backmenu.Name = "backmenu";
-            backmenu.Size = new Size(136, 37);
-            backmenu.TabIndex = 14;
             // 
             // bunifuElipse3
             // 
             bunifuElipse3.ElipseRadius = 20;
             bunifuElipse3.TargetControl = backmenu;
             // 
-            // AdminPanel
+            // AdminComplaints
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightSteelBlue;
             ClientSize = new Size(970, 473);
             Controls.Add(dashboard);
-            Controls.Add(backmenu);
-            Controls.Add(button1);
-            Controls.Add(label1);
             Controls.Add(linkLabel4);
             Controls.Add(linkLabel3);
             Controls.Add(linkLabel2);
+            Controls.Add(label1);
             Controls.Add(panel1);
+            Controls.Add(backmenu);
             FormBorderStyle = FormBorderStyle.None;
-            Name = "AdminPanel";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "AdminPanel";
+            Name = "AdminComplaints";
+            Text = "AdminComplaints";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -174,14 +161,13 @@
 
         private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
         private Panel panel1;
-        private Button button1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse2;
         private Label label1;
+        private LinkLabel dashboard;
+        private Panel backmenu;
         private LinkLabel linkLabel4;
         private LinkLabel linkLabel3;
         private LinkLabel linkLabel2;
-        private LinkLabel dashboard;
-        private Bunifu.Framework.UI.BunifuElipse bunifuElipse2;
-        private Panel backmenu;
         private Bunifu.Framework.UI.BunifuElipse bunifuElipse3;
     }
 }
